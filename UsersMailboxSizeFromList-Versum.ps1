ecad83b5cc6775e601ed917ceb595c088850fcb4
keelerd <keelerd@airproducts.com>
2016-03-31 09:11:29 -0400

    Added files via upload

---
+++ UsersMailboxSizeFromList-Versum.ps1
@@ -0,0 +1,19 @@
+﻿
+
+$list = Import-Csv C:\scripts\UserProvisioning-03.17-GoldList.csv
+$MbxSize = @()
+
+Foreach ($l in $lists){
+$MbxSize += (
+	Get-Mailbox -resultSize Unlimited $l.UserPrincipalNameAP | foreach{
+$mbx = $_ | select Name,UserPrincipalName,PrimarySMTPAddress
+Get-MailboxStatistics $_ | foreach{
+
+$mbx | add-member -type noteProperty -name TotalItemSizeMB -value $_.TotalItemSize -PassThru |`
+add-member -type noteProperty -name ItemCount -value $_.ItemCount -PassThru
+  }
+ }
+)
+} 
+
+$MbxSize | select Name,UserPrincipalName,PrimarySMTPAddress,ItemCount,TotalItemSizeMB | export-csv C:\Scripts\VersumMailboxSizes.csv -NoTypeInformation
